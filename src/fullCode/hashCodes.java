0f0e6a5d3042808a20dc05c1e3cd14f0e2a72a39
Surajdhage31 <surajdhage2006@gmail.com>
2024-01-30 23:20:10 +0530

    use of hash code

---
+++ src/fullCode/hashCodes.java
@@ -0,0 +1,13 @@
+package fullCode;
+
+public class hashCodes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   Integer i = new Integer(124);
+    int hashvalue = i.hashCode();
+    //return the hash code value of object
+    System.out.println(hashvalue);
+	}
+
+}
