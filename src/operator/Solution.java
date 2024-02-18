84cd021cbd3babd4934efe564bb6338664daf613
Surajdhage31 <surajdhage2006@gmail.com>
2024-02-18 21:35:07 +0530

    table of any number

---
+++ src/operator/Solution.java
@@ -0,0 +1,29 @@
+package operator;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+	public static void main(String[] args) {// throws IOException {
+		// BufferedReader bufferedReader = new BufferedReader(new
+		// InputStreamReader(System.in));
+
+		// int N = Integer.parseInt(bufferedReader.readLine().trim());
+		Scanner ram = new Scanner(System.in);
+		int N = ram.nextInt();
+		for (int i = 1; i<=10;i++) {
+		       
+				int result = N * i;
+				 
+				System.out.println("" + N + "*" + i + "=" + result);
+			
+				}
+		//bufferedReader.close();
+	}
+}
