c97622e9fa0f6e19bda2c66223030618e24cdc21
Surajdhage31 <surajdhage2006@gmail.com>
2024-02-21 20:58:37 +0530

    using if else find the leap year

---
+++ src/loop/ForLoop.java
@@ -0,0 +1,37 @@
+package loop;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year = 2024;
+		 boolean leap = false ;
+		 if (year%4==0) {
+		 if(year%100==0)
+		 {if(year%400==0) {
+			 leap = true ;
+		 }
+		 else
+		 
+			 leap = false ;
+		 }
+		 else
+		 
+			 leap = true ;
+		 } 
+		 else
+		 {
+			leap = false ;
+		 }
+	   if(leap)
+	   {
+		   System.out.println(year+" is a leap year ");
+		 }
+	   else
+	   {
+	   System.out.println(year+" is not leap");
+	}
+	
+	}
+
+}
