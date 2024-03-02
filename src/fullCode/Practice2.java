be48986addc4c107feb71886a1078a98f72d0d38
Surajdhage31 <surajdhage2006@gmail.com>
2024-03-02 23:56:02 +0530

    create the pattern using continue and break

---
+++ src/fullCode/Practice2.java
@@ -0,0 +1,17 @@
+package fullCode;
+
+public class Practice2 {
+	public static void main(String[] args) {
+	int n =5 ;
+	for(int a = n;a>=1;a--)
+	{
+		for(int i = 1;i<=a;i++)
+		{
+			System.out.print("*");
+		}
+		System.out.println(" ");
+	}
+			}
+		}
+	
+
